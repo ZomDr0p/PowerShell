ea1fcc08545009f6f5f603edb12c205385ccef4b
ZomDr0p <60549107+ZomDr0p@users.noreply.github.com>
2021-02-26 20:34:26 +0100

    Shows the OS version and Build

---
+++ OwnCrafted/OSIdentifier.ps1
@@ -0,0 +1,7 @@
+$os = Get-WmiObject -class win32_operatingsystem
+
+$objecto = New-Object -TypeName PSObject;
+$objecto | Add-Member -MemberType NoteProperty -Name "OSBuild" -Value $os.BuildNumber;
+$objecto | Add-Member -MemberType NoteProperty -Name "WinVersion" -Value $os.Version;
+
+Write-Host $objecto
