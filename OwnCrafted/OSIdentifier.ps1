f34bd05bb46b7647a89dac9d9ab923059fca3237
ZomDr0p <60549107+ZomDr0p@users.noreply.github.com>
2021-02-26 20:47:42 +0100

    Upgrade the output to be more visible

--- OwnCrafted/OSIdentifier.ps1
+++ OwnCrafted/OSIdentifier.ps1
@@ -4,4 +4,4 @@ $objecto = New-Object -TypeName PSObject;
 $objecto | Add-Member -MemberType NoteProperty -Name "OSBuild" -Value $os.BuildNumber;
 $objecto | Add-Member -MemberType NoteProperty -Name "WinVersion" -Value $os.Version;
 
-Write-Host $objecto
+Write-Output $objecto
